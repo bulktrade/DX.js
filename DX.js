d2bdff8ece888f86aad106c3fbeda31e8ce839d7
Sergej Kunz <info@bulktrade.org>
2015-07-20 00:24:24 +0200

    fix

--- DX.js
+++ DX.js
@@ -1,30 +1,28 @@
 var DX = {
-    Common: {
-        EscapeString: function (val) {
-            val = val.replace(/[\0\n\r\b\t\\'"\x1a]/g, function (s) {
-                switch (s) {
-                    case "\0":
-                        return "\\0";
-                    case "\n":
-                        return "\\n";
-                    case "\r":
-                        return "\\r";
-                    case "\b":
-                        return "\\b";
-                    case "\t":
-                        return "\\t";
-                    case "\x1a":
-                        return "\\Z";
-                    case "'":
-                        return "''";
-                    case '"':
-                        return '""';
-                    default:
-                        return "\\" + s;
-                }
-            });
+    escapeString: function (val) {
+        val = val.replace(/[\0\n\r\b\t\\'"\x1a]/g, function (s) {
+            switch (s) {
+                case "\0":
+                    return "\\0";
+                case "\n":
+                    return "\\n";
+                case "\r":
+                    return "\\r";
+                case "\b":
+                    return "\\b";
+                case "\t":
+                    return "\\t";
+                case "\x1a":
+                    return "\\Z";
+                case "'":
+                    return "''";
+                case '"':
+                    return '""';
+                default:
+                    return "\\" + s;
+            }
+        });
 
-            return val;
-        }
+        return val;
     }
 };
